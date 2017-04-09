293cb13aea78b0258d4c9d2af6a5f730dd427848
Mohammed <mgomar94@gmail.com>
2017-04-08 19:44:34 -0700

    Add files via upload
    Initializing the repo

---
+++ Servo_Motor.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+Servo servo1;
+
+void setup()
+{
+  servo1.attach(8);
+}
+void loop()
+{
+  servo1.write(820);
+  delay(800);
+  servo1.write(1500);
+  delay(800);
+  servo1.write(2210);
+  delay(800);
+} 
+
